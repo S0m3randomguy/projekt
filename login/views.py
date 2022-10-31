d8e1cd960968335effa305dea11199bc4b8c6ec2
S0m3randomguy <Slayerlegend54@gmail.com>
2022-10-31 21:15:14 +0100

    Passed language as context argument in login view.

--- login/views.py
+++ login/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import *
+from .language import Language
 
 LOGIN_FILE = "login/main.html"
+LANGUAGE   = "en-US" # [PLACEHOLDER]
 
 def home(request):
     return HttpResponseRedirect("/login")
 
 def login(request):
-    context = {}
-    context["login_form"] = LoginForm()
-    context["register_form"] = RegisterForm()
+    context = {
+        "login_form"    : LoginForm(),
+        "register_form" : RegisterForm(),
+        "language"      : Language(LANGUAGE)
+    }
     return render(request or None, LOGIN_FILE, context)
