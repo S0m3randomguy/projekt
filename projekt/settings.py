78045b8ee7e6cd6622b47151b976953304eb564f
S0m3randomguy <Slayerlegend54@gmail.com>
2022-11-01 15:26:35 +0100

    Moved static file directory to 'assets' directory.

--- projekt/settings.py
+++ projekt/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "assets/static/"),
 )
 
 # Default primary key field type
