eecdf89f9303ed2bc8aa12f4a9fa502486a24de6
S0m3randomguy <Slayerlegend54@gmail.com>
2022-11-18 13:16:35 +0100

    Added changing language via GET request.

--- login/views.py
+++ login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import *
 from projekt.language import *
+import os
 
 LOGIN_FILE = "login/main.html"
 
@@ -9,9 +10,15 @@ def home(request):
     return HttpResponseRedirect("/login")
 
 def login(request):
-    context = {
-        "login_form"    : LoginForm(),
-        "register_form" : RegisterForm(),
-        "language"      : Language(LANGUAGE)
-    }
-    return render(request or None, LOGIN_FILE, context)
+    if request.method == "GET":
+        language = request.GET.get("lang", None)
+        context = {
+            "login_form"    : LoginForm(),
+            "register_form" : RegisterForm(),
+            "language"      : Language(verify_language(language) or "en-US")
+        }
+        return render(request or None, LOGIN_FILE, context)
+
+def verify_language(lang: str):
+    languages = [name for name in os.listdir("assets/languages")]
+    return lang if lang in languages else None
