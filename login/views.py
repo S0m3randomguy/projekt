a726cb0087011c753da8ece387cc8b7dff08e3b7
S0m3randomguy <Slayerlegend54@gmail.com>
2022-11-24 19:59:01 +0100

    Revamped login and registration pages. Added to urls.

--- login/views.py
+++ login/views.py
@@ -4,34 +4,37 @@ from .forms import *
 from projekt.language import *
 import os
 
-LOGIN_FILE = "login/main.html"
-GET_FORM = {
-    "login"     : get_login_form,
-    "register"  : get_register_form
-}
+LOGIN_FILE      = "login/main.html"
+REGISTER_FILE   = "register/main.html"
+
+def form_method(func, template):
+    def method(request):
+        lang = request.GET.get("lang", None)
+        language = Language(verify_language(lang) or "en-US")
+
+        if request.method == "POST":
+            form = func(language, request)
+            if form.is_valid():
+                # todo
+                return HttpResponseRedirect("/test")
+
+        context = {
+            "login_form"    : get_login_form(language),
+            "language"      : language
+        }
+        return render(request or None, template, context)
+    return method
 
 def home(request):
     return HttpResponseRedirect("/login")
 
 def login(request):
-    lang = request.GET.get("lang", None)
-    language = Language(verify_language(lang) or "en-US")
+    method = form_method(get_login_form, LOGIN_FILE)
+    return method(request)
 
-    if request.method == "POST":
-        form_type = request.POST.get("form", None)
-        form = GET_FORM.get(form_type, get_login_form)(language, request.POST)
-
-        if form.is_valid():
-            # todo
-            return HttpResponseRedirect("/test")
-
-    context = {
-        "login_form"    : get_login_form(language),
-        "register_form" : get_register_form(language),
-        "language"      : language
-    }
-    return render(request or None, LOGIN_FILE, context)
-
+def register(request):
+    method = form_method(get_register_form, REGISTER_FILE)
+    return method(request)
 
 def verify_language(lang: str):
     languages = [name for name in os.listdir("assets/languages")]
