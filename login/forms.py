f5e64da9a3cf5c757ffbb222020d13859e695927
S0m3randomguy <Slayerlegend54@gmail.com>
2022-12-01 20:35:26 +0100

    Addded custom field validation to RegisterForm.

--- login/forms.py
+++ login/forms.py
@@ -1,9 +1,28 @@
 from django import forms
 from projekt.language import *
 from .models import Account
+from configparser import ConfigParser
+from validators import (
+    MinLengthValidator,
+    MaxLengthValidator,
+    CharsetValidator,
+    EmailValidator,
+    ExtendedAsciiValidator
+)
+import string
 
-MIN_USERNAME_LENGTH = 3
-MAX_USERNAME_LENGTH = 20
+CONFIG_FILE = "assets/config.ini"
+config = ConfigParser(CONFIG_FILE)
+
+ACCOUNT_SECTION     = "account"
+MIN_NAME_LENGTH     = config.get(ACCOUNT_SECTION, "MIN_NAME_LENGTH")
+MAX_NAME_LENGTH     = config.get(ACCOUNT_SECTION, "MAX_NAME_LENGTH")
+
+MIN_USERNAME_LENGTH = config.get(ACCOUNT_SECTION, "MIN_USERNAME_LENGTH")
+MAX_USERNAME_LENGTH = config.get(ACCOUNT_SECTION, "MAX_USERNAME_LENGTH")
+
+MIN_PASSWORD_LENGTH = config.get(ACCOUNT_SECTION, "MIN_PASSWORD_LENGTH")
+MAX_PASSWORD_LENGTH = config.get(ACCOUNT_SECTION, "MIN_PASSWORD_LENGTH")
 
 def get_login_form(lang: Language, request=None):
     SECTION_LOGIN        = lang.sections["login"]
@@ -69,6 +88,50 @@ def get_register_form(lang: Language, request=None):
                     "placeholder"   : CONFIRMATION_PLACEHOLDER,
                     "id"            : "confirmation_field"
                 })
+            }
+            validators = {
+                "name": [
+                    MinLengthValidator(
+                        lang, FULL_NAME_PLACEHOLDER,
+                        MIN_NAME_LENGTH
+                    ),
+                    MaxLengthValidator(
+                        lang, FULL_NAME_PLACEHOLDER,
+                        MAX_NAME_LENGTH
+                    ),
+                ],
+                "username": [
+                    MinLengthValidator(
+                        lang, USERNAME_PLACEHOLDER,
+                        MIN_USERNAME_LENGTH
+                    ),
+                    MaxLengthValidator(
+                        lang, USERNAME_PLACEHOLDER,
+                        MAX_USERNAME_LENGTH
+                    ),
+                    CharsetValidator(
+                        lang, USERNAME_PLACEHOLDER,
+                        string.ascii_letters + string.digits + "_"
+                    )
+                ],
+                "email": [
+                    EmailValidator(
+                        lang, EMAIL_PLACEHOLDER
+                    )
+                ],
+                "password": [
+                    MinLengthValidator(
+                        lang, PASSWORD_PLACEHOLDER,
+                        MIN_PASSWORD_LENGTH
+                    ),
+                    MaxLengthValidator(
+                        lang, PASSWORD_PLACEHOLDER,
+                        MAX_PASSWORD_LENGTH
+                    ),
+                    ExtendedAsciiValidator(
+                        lang, PASSWORD_PLACEHOLDER
+                    )
+                ]
             }
 
         def clean(self):
